349857c039c60a82d512a2d3802d9c1c8630848f
Duong Nguyen <duongital@gmail.com>
2020-02-20 18:42:06 +0700

    remove css

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <h1>{{ msg }}</h1>
     <h2>to do:</h2>
     <ul>
+        <li>add tailwind css</li>
         <li>home with canvas background</li>
         <li>blogs: hook with firebase store</li>
         <li>cv</li>
@@ -15,33 +16,8 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to @duongital blog'
     }
   }
 }
 </script>
-
-<style{{#sass}} lang="scss"{{/sass}}>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
