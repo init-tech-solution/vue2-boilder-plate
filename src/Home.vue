222c6f8855928afe106c1b1ae3ce8ed7b0078adb
Duong Nguyen <duongital@gmail.com>
2020-02-22 12:19:35 +0700

    add canvas

--- src/Home.vue
+++ src/Home.vue
@@ -1,18 +1,24 @@
 <template>
-  <div style="margin-top: 30%">
-    <h1 class="text-center">{{ msg }}</h1>
-    <div class="text-center">
-      <h2 class="text-cyan">to do:</h2>
-      <ul>
-        <li>home with canvas background</li>
-        <router-link to="/blog">
+  <div
+    style="width: 100vw; height: 100vh; positioin: relative"
+  >
+    <div style="position: absolute; width: 500px; height: 100px; top: 0; left: 0; right: 0; bottom: 0; margin: auto">
+      <h1 class="text-center">{{ msg }}</h1>
+      <p>không code nữa, Dương mệt rồi...</p>
+      <div class="text-right">
+        <h2 class="text-cyan">to do:</h2>
+        <ul>
+          <li>home with canvas background</li>
+          <router-link to="/blog">
             <li class="underline">blogs: hook with firebase store</li>
-        </router-link>
-        <router-link to="/cv">
+          </router-link>
+          <router-link to="/cv">
             <li class="underline">cv</li>
-        </router-link>
-      </ul>
+          </router-link>
+        </ul>
+      </div>
     </div>
+    <canvas id="canvas" style="width: 100vw; height: 100vh"></canvas>
   </div>
 </template>
 
@@ -23,6 +29,14 @@ export default {
     return {
       msg: "welcome to @duongital blog"
     };
+  },
+  mounted() {
+      const canvas = document.getElementById("canvas");
+      const ctx = canvas.getContext("2d");
+
+      ctx.fillStyle = "coral";
+      ctx.fillRect(20, 20, 150, 100);
   }
+
 };
 </script>
